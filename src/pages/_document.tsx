9ce856c9ee888a6eaf794e72cac217712dffe069
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-07-15 11:55:42 -0300

    feat: finish _document.tsx file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render(): JSX.Element {
+  render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
